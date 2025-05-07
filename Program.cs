aa90726a115ddbd0b621d754b7aca1c0746a07cb
Isac Epifanio <isacepifanio4@gmail.com>
2025-05-06 21:18:32 -0300

    docker

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ using Thoughts.services.thought.thought;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 8080); // Porta 8080
+});
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -51,10 +56,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.Listen(IPAddress.Any, 8080); // Porta 8080
-});
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
